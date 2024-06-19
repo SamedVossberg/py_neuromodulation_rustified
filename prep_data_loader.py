1644386b73b175de0c7b05b377862cd216f9a192
timonmerk <timon.merk95@gmail.com>
2024-06-19 16:54:26 +0200

    remove subs without dysk

--- prep_data_loader.py
+++ prep_data_loader.py
@@ -2,8 +2,9 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-
+#
 PATH_DATA = r"E:\Downloads\all_merged_normed_rmap.csv"
+PATH_DATA = r"C:\Users\ICN_GPU\Downloads\all_merged_normed_rmap.csv"
 
 df = pd.read_csv(PATH_DATA)
 
@@ -19,11 +20,26 @@ df_all = df_all.dropna(axis=1)
 df_all = df_all.fillna(0)
 
 subs = np.sort(df_all["sub"].unique())
+subs_no_dyk = ["rcs10", "rcs14", "rcs15", "rcs19"]
+subs = np.array(
+    [s for s in subs if not any([s_no for s_no in subs_no_dyk if s_no in s])]
+)
+# remove all entries where sub is not in subs
+df_all = df_all[df_all["sub"].isin(subs)]
+
 subs_idx = np.arange(subs.shape[0])
 df_all["sub_idx"] = df_all["sub"].apply(lambda x: np.where(subs == x)[0][0])
 
-cols_add = [f for f in df_all.columns if f.startswith("cortex") or f == "sub_idx" or f == "pkg_dk_class" 
-                or f == "pkg_dk_normed" or f == "pkg_dt" or f == "cortex_hour"]
+cols_add = [
+    f
+    for f in df_all.columns
+    if f.startswith("cortex")
+    or f == "sub_idx"
+    or f == "pkg_dk_class"
+    or f == "pkg_dk_normed"
+    or f == "pkg_dt"
+    or f == "cortex_hour"
+]
 # swap column position 269 and 266
 cols_add[269], cols_add[266] = cols_add[266], cols_add[269]
 
@@ -43,9 +59,11 @@ for sub_idx, sub in enumerate(subs):
     idx_sub.append(list(idx_30min))
     print(f"sub: {sub}, num_series: {len(idx_30min)}\n")
     for idx in idx_30min:
-        feature_arr.append(
-            df_sub.loc[idx - 14:idx, :][cols_add].values
-        )
+        feature_arr.append(df_sub.loc[idx - 14 : idx, :][cols_add].values)
 
 f_arr = np.array(feature_arr)
-np.save(r"E:\Downloads\all_merged_normed_rmap.npy", f_arr, )
+np.save(
+    # r"E:\Downloads\all_merged_normed_rmap.npy",
+    r"C:\Users\ICN_GPU\Downloads\all_merged_normed_rmap.npy",
+    f_arr,
+)
